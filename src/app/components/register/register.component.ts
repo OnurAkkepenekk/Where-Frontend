cc32ec3cb87677131ed5a0b9ce67f9ee00d9bc8b
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-27 23:41:40 +0300

    Create register operations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { RegisterService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private registerService: RegisterService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
-
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  register() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      let loginModel = Object.assign({}, this.registerForm.value);
+      this.registerService.register(loginModel).subscribe(
+        (response) => {
+          this.toastrService.info(response.message);
+          console.log(response);
+        },
+        (responseError) => {
+          console.log(responseError);
+          this.toastrService.error(responseError.error);
+        }
+      );
+    }
+  }
 }
---
+++ src/app/services/register.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterModel } from '../models/register';
+import { SingleResponseModel } from '../models/singleResponseModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  apiUrl = 'https://localhost:44380/api/auth/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  register(user: RegisterModel) {
+    return this.httpClient.post<SingleResponseModel<RegisterModel>>(
+      this.apiUrl + 'register',
+      user
+    );
+  }
+}
