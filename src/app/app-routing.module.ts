096a97bb3c9f4ff0b226c57d46d05f263a40114b
152120171032 <thehuseyinoglu@gmail.com>
2021-05-30 16:04:51 +0300

    Add mainpage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'mainpage',
+    component: MainpageComponent,
   },
 ];
 
