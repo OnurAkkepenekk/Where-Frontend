a030ebf104ecd96a1bcda484186d15f8bcf97d58
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-06-01 10:57:15 +0300

    Create ProfileComponent

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginModel } from 'src/app/models/loginModel';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user:LoginModel
+  constructor(private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.getInfos("");
+    //email nasıl alınmalı?
+  }
+  getInfos(email:string){
+    this.userService.getByMail(email);
+  }
+}
