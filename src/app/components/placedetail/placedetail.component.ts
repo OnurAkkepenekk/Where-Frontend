ecd7a983bad3e8177d26b6a6690c790d884db7b4
152120171032 <thehuseyinoglu@gmail.com>
2021-06-01 15:49:21 +0300

    Create placedetail component

---
+++ src/app/components/placedetail/placedetail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placedetail',
+  templateUrl: './placedetail.component.html',
+  styleUrls: ['./placedetail.component.css']
+})
+export class PlacedetailComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
