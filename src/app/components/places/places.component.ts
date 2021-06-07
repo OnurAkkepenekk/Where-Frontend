b10f00d75ec7091e510b496a83502ba7a16fbcca
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-07 06:17:06 +0300

    Update place components

--- src/app/components/places/places.component.ts
+++ src/app/components/places/places.component.ts
@@ -1,5 +1,8 @@
+import { GroupsEntity, ItemsEntity, Venue } from './../../models/FourSquare';
 import { FoursquareService } from './../../services/foursquare.service';
 import { Component, OnInit } from '@angular/core';
+import { FourSquare, NeOrSw } from 'src/app/models/FourSquare';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-places',
@@ -8,8 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesComponent implements OnInit {
   location: string;
-  groups:any;
-  items:any;
+  locations =[{
+    lat:1,
+    lng:1,
+  }];
+  listOfLocations=[{}];
   constructor(private foursquareAPI: FoursquareService) {}
   ngOnInit(): void {
     this.getLocation();
@@ -24,8 +30,17 @@ export class PlacesComponent implements OnInit {
   }
   getPlaces(location: string) {
     console.log(location);
-    this.foursquareAPI
-      .getPlaces(location)
-      .subscribe((response) => console.log(response.response.groups[0].items[0].venue.name));
+    this.foursquareAPI.getPlaces(location).subscribe((response) => {
+      for (let index = 0; index < response.response.groups[0].items.length; index++) {
+        this.locations[index] = response.response.groups[0].items[index].venue.location;
+        this.listOfLocations= response.response.groups[0].items;
+      }
+      console.log(this.locations);
+      console.log("***********************");
+      console.log(this.listOfLocations);
+
+      // this.listOfLocations.response.groups[0].items[i].venue
+      // this.locations.response.groups[0].items(response.response.groups[0].items[0].venue.location.lat);
+    });
   }
 }
